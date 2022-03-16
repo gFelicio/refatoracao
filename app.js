b502b0a3cee6393f77f85d84bb9ef7c990a1a451
gFelicio <gustavo.a.felicio@gmail.com>
2022-03-16 23:43:30 +0000

    express

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 const express = require("express")
 const app = express()
+const rotas = express.Router()
 const porta = process.env.PORT = 3000
+const path = require('path')
+
+app.use(rotas)
+app.use(express.json())
+
+app.set('views', path.join(__dirname, './'))
+app.set('view engine', 'ejs' )
+
+rotas.get('/', (req, res) => {
+    res.send('Rota raiz encontrada')
+})
+
+rotas.get('/ola', (req, res) => {
+    res.send('Rota Olá encontrada')
+})
+
+rotas.get('/compras', (req, res) => {
+    res.send('Rota Compras encontrada')
+})
+
+rotas.get('/index', (req, res) => {
+    // res.sendFile(path.join(__dirname + '/index.html'))
+    res.render(path.join(__dirname + '/index.ejs'), {
+        title: 'AAAAAAAAAAA',
+        mensagem: "Mensagem",
+        titulo: "Título aqui",
+        autor: "Gustavo Felício"
+    })
+})
+
 app.listen(porta, () => {
     console.log('Server na porta', porta)
 });
